cc5c0058bdc0b775b3adb6a61c2d9a85f6abb211
Rish1902 <103328929+Rish1902@users.noreply.github.com>
2022-06-18 17:05:01 +0530

    Add files via upload

---
+++ main (1).c
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#define n 6
+int main(){
+    int arr[n] = {1, 2, 3, 4, 5, 6};
+    int temp;
+    for(int i = 0; i<n/2; i++){
+        temp = arr[i];
+        arr[i] = arr[n-i-1];
+        arr[n-i-1] = temp;
+    }
+    for(int i = 0; i < n; i++){
+        printf("%d,", arr[i]);
+    }
+    return 0;
+}
+
